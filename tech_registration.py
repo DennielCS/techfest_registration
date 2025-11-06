0cc0804cae3610c7580ccbd405176c74d324379a
Denniel Marth Enraca <dennielmarth.enraca@benilde.edu.ph>
2025-11-06 12:06:44 +0800

    Implemented Registration Setup

---
+++ tech_registration.py
@@ -0,0 +1,8 @@
+print("Welcome to SMIT TechFest!")
+print("Organized by Denniel Enraca of APPDAET BTCS2")
+print()
+
+participants = int(input("How many participants will register? "))
+
+if participants <= 0:
+    print("Invalid number of participants.")
