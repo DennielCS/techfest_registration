bcda3671364579ed237d55111f320fa73a689435
Denniel Marth Enraca <dennielmarth.enraca@benilde.edu.ph>
2025-11-06 12:14:16 +0800

    Added Participant Information Collection

--- tech_registration.py
+++ tech_registration.py
@@ -2,7 +2,20 @@ print("Welcome to SMIT TechFest!")
 print("Organized by Denniel Enraca of APPDAET BTCS2")
 print()
 
-participants = int(input("How many participants will register? "))
+number_of_participants = int(input("How many participants will register? "))
 
-if participants <= 0:
+if number_of_participants <= 0:
     print("Invalid number of participants.")
+else:
+    participants = []
+
+    for i in range(number_of_participants):
+        name = input("Enter participant name: ")
+        track = input("Enter chosen track: ")
+
+        participant = {"name": name, "track": track}
+        participants.append(participant)
+
+    print("\nRegistered Participants:")
+    for i, participant in enumerate(participants, 1):
+        print(f"{i}. {participant['name']} - {participant['track']}")
