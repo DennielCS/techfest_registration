98a1a3dd2c21e86aeaf52cbb39f836b3fc5f6508
Denniel Marth Enraca <dennielmarth.enraca@benilde.edu.ph>
2025-11-06 12:19:00 +0800

    Added Track Diversity Report

--- tech_registration.py
+++ tech_registration.py
@@ -1,9 +1,11 @@
+# Task 1
 print("Welcome to SMIT TechFest!")
 print("Organized by Denniel Enraca of APPDAET BTCS2")
 print()
 
 number_of_participants = int(input("How many participants will register? "))
 
+# Task 2
 if number_of_participants <= 0:
     print("Invalid number of participants.")
 else:
@@ -19,3 +21,15 @@ else:
     print("\nRegistered Participants:")
     for i, participant in enumerate(participants, 1):
         print(f"{i}. {participant['name']} - {participant['track']}")
+
+# Task 3
+    unique_tracks = set()
+    for participant in participants:
+        unique_tracks.add(participant['track'])
+
+    tracks_list = sorted(list(unique_tracks))
+    print("\nTracks offered in this event:")
+    print(", ".join(tracks_list))
+
+    if len(unique_tracks) < 2:
+        print("Not enough variety in tracks.")
